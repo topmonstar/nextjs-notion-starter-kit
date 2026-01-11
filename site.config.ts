86a1bb2a1d9cc63e766f330cbe75273e96303b64
topmonstar <papayabear@hotmail.com>
2026-01-11 16:35:26 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2e5d3784b7ce81298c44ed925561fe0d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '星辰万相',
+  domain: 'cloudblog.de5.net',
+  author: 'Z大',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '知识的海洋浩若星辰，鱼跃于天，星辰大海，在这里记录他们！',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
